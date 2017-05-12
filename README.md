2ede983bd0a7e21954eae843cb6adc3b4c3205af
Stanislau_Halauniou <Stanislau_Halauniou@epam.com>
2017-05-12 14:58:08 +0300

    Add ascii doctor plugin

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Add classpath("com.epam.petclinic.plugin:petclinic-plugins:0.0.1") to your scrip
 For java plugin add - apply plugin: 'petclinic-java' (remove version, sourceCompatibility ?) - it adds java, checkstyle,
 pmd and findbugs plugins
 For code coverage add - apply plugin: 'petclinic-code-coverage' and jacocoVersion=0.7.8(to gradle.properties)
-For using idea plugin - apply plugin: 'petclinic-idea', it applies configurations for idea for
+For using idea plugin add - apply plugin: 'petclinic-idea', it applies configurations for idea for
 checkstyle, formatter, pmd, findbugs plugins(BUT in addition: CheckStyle-IDEA, PMDPlugin, FindBugs-IDEA should be installed on ide)
+For use document generation plugin for rest docs - apply plugin: 'petclinic-ascii-doctor'
---
+++ src/main/groovy/com/epam/petclinic/plugin/AsciiDoctorPlugin.groovy
@@ -0,0 +1,32 @@
+package com.epam.petclinic.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+/**
+ * Plugin generates Restfull documentation based on Rest Docs tests.
+ *
+ * Date: 5/12/2017
+ *
+ * @author Stanislau Halauniou
+ */
+class AsciiDoctorPlugin implements Plugin<Project> {
+
+    private static final String REST_DOCS_PLUGIN = 'org.asciidoctor.convert'
+
+    @Override
+    void apply(Project project) {
+        project.plugins.apply(REST_DOCS_PLUGIN)
+        project.tasks['jar'].dependsOn(project.tasks['asciidoctor'])
+        def snippetsDir = project.file('build/generated-snippets')
+
+        project.asciidoctor {
+            attributes 'snippets': snippetsDir
+            inputs.dir snippetsDir
+            outputDir "build/asciidoc"
+            dependsOn project.tasks['test']
+            sourceDir 'src/main/resources/asciidoc'
+        }
+    }
+}
